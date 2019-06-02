3b9db40fe1dbce2c133043b7f04bb2990ec3278c
Adam <adamorlowskipoland@icloud.com>
2019-06-02 10:56:18 +0200

    mount vs shallowMount check

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -1,6 +1,17 @@
-import { mount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import TestComponent from '@/test';
+import List from '@/list';
 
-test('first-spec', () => {
-  console.log(TestComponent);
+test('mount a vue component', () => {
+  const wrapper = mount(TestComponent, {
+    propsData: {
+      value: 'Test prop value'
+    }
+  });
+  expect(wrapper.html()).toMatchSnapshot()
+});
+
+test('ListComponent Shallow', () => {
+  console.log(mount(List).html());
+  console.log(shallowMount(List).html());
 });
